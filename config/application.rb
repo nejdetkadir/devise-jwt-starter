3e9b36923787433440ea2a724e32bd417b4d1d99
nejdetkadir <nejdetkadir.550@gmail.com>
2021-11-26 23:17:32 +0300

    Configure rails generators for using primary key as uuid

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,10 @@ module DeviseJwtStarter
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
